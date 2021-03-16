5463ac56f1700b9acacc0ce7152c19ce6f9ae581
CK-Tu <xut@Appen.com>
2021-03-16 08:43:45 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,38 @@
   <img src="./assets/img/logo.png">
   <div>
     <div id="title"></div>
-    <editor id="editor"></editor>
-    <div id="show"></div>
+    <!-- <editor id="editor" ></editor> -->
+    <div id="editor" class="editor_area">
+      <editor @my-emit="parentEmit" ></editor>
+    </div>
+    
+    <div id="show" class="show_area">
+      <textarea v-model="a" class="show_style"></textarea>
+    </div>
+    
   </div>
 </template>
 
 <script>
-
+import {ref, defineComponent} from 'vue'
 import editor from './components/editor.vue';
-export default {
+export default defineComponent({
   name: 'markDownApp',
   components: {
     editor
-  }
-}
+  },
+  setup() {
+    let a = ref("")
+    function parentEmit(val){
+        // alert(val)
+        a.value = val
+    }
+    return{
+        parentEmit,
+        a
+    }
+  },
+});
 </script>
 
 <style>
@@ -26,5 +44,24 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.editor_area {
+  width: 50%;
+  height:500px;
+  float: left;
+
+}
+.editor_area div{
+  width:100%;
+  height: 100%;
+}
+.show_area{
+  width: 50%;
+  height: 500px;
+  float: left;
+}
+.show_style{
+  width:100%;
+  height: 100%;
 }
 </style>
